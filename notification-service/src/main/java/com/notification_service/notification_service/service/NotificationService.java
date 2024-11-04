0fa5345a8d109a0cfe9ac9cd63cdfe1d11768a0e
beyhosni <beyhosni@gmail.com>
2024-11-04 11:57:06 +0100

    update

--- notification-service/src/main/java/com/notification_service/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/notification_service/notification_service/service/NotificationService.java
@@ -1,23 +1,35 @@
 package com.notification_service.notification_service.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
     @KafkaListener(topics = "order-events", groupId = "notification-group")
     public void handleNotification(String message) {
-        // Logique pour traiter le message et envoyer la notification
-        System.out.println("Notification received: " + message);
-        
-        // Par exemple : envoi d'email ou SMS
-        // sendEmailNotification(message);
+        try {
+            System.out.println("Notification received: " + message);
+            sendEmailNotification(message);
+        } catch (Exception e) {
+            System.err.println("Erreur lors de l'envoi de la notification: " + e.getMessage());
+        }
     }
     
     // Méthode pour envoyer un email (exemple)
     public void sendEmailNotification(String message) {
-        // Logique d'envoi d'email
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo("destinataire@example.com");
+        mailMessage.setSubject("Nouvelle notification");
+        mailMessage.setText(message);
+        
+        mailSender.send(mailMessage);
     }
 }
 
